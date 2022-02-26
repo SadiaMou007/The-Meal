ea8edb38fa1e4c12f8aabe2ad5bb5a4c6b53200a
Your Name <you@example.com>
2022-02-26 14:07:13 -0800

    add details

--- js/app.js
+++ js/app.js
@@ -22,7 +22,10 @@ const searchbtn = () => {
 
 // show search result
 const getMeals = (data) => {
+  showTotalItem(data.length);
+  console.log(data.length);
   const mealContainer = document.getElementById("mealContainer");
+  mealContainer.innerHTML = "";
   for (const meal of data) {
     const div = document.createElement("div");
     div.innerHTML = `
@@ -30,16 +33,14 @@ const getMeals = (data) => {
          <div class="card">
              <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
              <div class="card-body">
-                 <h5 class="card-title">${meal.strMeal}</h5>
+                 <h5 class="card-title text-success">${meal.strMeal}</h5>
                  
              </div>
           </div>
         </div>
 `;
     mealContainer.appendChild(div);
-    // console.log(meal.strMeal);
   }
-  //   console.log(data);
 };
 
 function loaddetail(mealId) {
@@ -55,24 +56,37 @@ const showDetail = (data) => {
   console.log(data.meals[0]);
   const div = document.createElement("div");
   div.innerHTML = `
-    <div class="card my-3 mx-auto" style="max-width: 540px;" id="detailCard">
-        <i class="fa-solid fa-xmark ms-auto p-2" onclick="closeDetails()"></i>
+    <div class="card container" id="detailCard">
+    <div class="bg-white m-auto w-75">
+   <i class="fa-solid fa-xmark p-2 closeIcon" onclick="closeDetails()"></i>
+    
+    <div class="row g-0">
+    <div class="col-md-4">
+         <img src="${data.meals[0].strMealThumb}" 
+         class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title text-success">${data.meals[0].strMeal} </h5>
+            <span class="text-info">Main Ingredient: </span>
+            <ul>
+            <li>${data.meals[0].strIngredient1}</li>
+            <li>${data.meals[0].strIngredient2}</li>
+            <li>${data.meals[0].strIngredient3}</li>
 
-         <div class="row g-0">
-            <div class="col-md-4">
-                 <img src="${data.meals[0].strMealThumb}" 
-                 class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">${data.meals[0].strMeal} </h5>
-                    <p class="card-text">${data.meals[0].strInstructions.slice(
-                      0,
-                      200
-                    )}</p>
-                </div>
-            </div>
-         </div>
+
+            </ul>
+            <p class="card-text"><span class="font-bold text-info">Instruction:</span> ${data.meals[0].strInstructions.slice(
+              0,
+              200
+            )}</p>
+            <a href="${
+              data.meals[0].strYoutube
+            }" class="btn btn-success">Full Recepie on Youtube</a>
+        </div>
+    </div>
+ </div>
+    </div>    
     </div>
   `;
   modal.appendChild(div);
@@ -80,4 +94,9 @@ const showDetail = (data) => {
 const closeDetails = () => {
   const modal = document.getElementById("modalC");
   modal.innerHTML = "";
+};
+// extra
+const showTotalItem = (values) => {
+  const text = `Total Item: ${values}`;
+  error.innerText = text;
 };
