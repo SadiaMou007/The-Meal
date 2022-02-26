ac8220cc2c6c8fb9d6bb488a5c25c5c938cc7d53
Your Name <you@example.com>
2022-02-26 06:28:01 -0800

    show search result

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// get search result
 const searchbtn = () => {
   const searchId = document.getElementById("search-field");
   const error = document.getElementById("error");
@@ -11,6 +12,32 @@ const searchbtn = () => {
   //for valid input
   else {
     error.innerText = "";
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchId.value}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => getMeals(data.meals));
   }
   searchId.value = "";
+};
+
+// show search result
+const getMeals = (data) => {
+  const mealContainer = document.getElementById("mealContainer");
+  for (const meal of data) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+     <div class="col">
+         <div class="card">
+             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+             <div class="card-body">
+                 <h5 class="card-title">${meal.strMeal}</h5>
+                 
+             </div>
+          </div>
+        </div>
+`;
+    mealContainer.appendChild(div);
+    console.log(meal.strMeal);
+  }
+  console.log(data);
 };
